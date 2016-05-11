4c1dffa807bc0ae35540753c953ee3323f947895
Eduardo G - odraudek99 <odraudek99@gmail.com>
2016-05-11 01:26:30 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bots-twitter-python
---
+++ bot_twitter_topics.py
@@ -0,0 +1,63 @@
+'''
+Created on May 5, 2016
+
+@author: odraudek99
+'''
+
+import tweepy
+import configparser
+import smtplib
+#from tweepy.auth import OAuthHandler
+
+
+config = configparser.RawConfigParser()
+config.read('/home/odraudek99/config.properties')
+
+password= config.get('correo', 'password')
+correoSalida= config.get('correo', 'correoSalida')
+correoDestino= config.get('correo', 'correoDestino')
+internetAddress= config.get('correo', 'internetAddress')
+
+consumer_key = config.get('twitter', 'consumer_key')
+consumer_secret = config.get('twitter', 'consumer_secret')
+access_token = config.get('twitter', 'access_token')
+access_token_secret = config.get('twitter', 'access_token_secret')
+resultados = config.get('twitter', 'resultados')
+
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+mensaje_correo=""
+
+try:
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    
+    search_results = api.search(q="linea12 OR linea dorada OR lineaDorada", count=resultados)
+    
+    for i in search_results:
+        mensaje_correo+=(str(i.created_at)+'\n')
+        mensaje_correo+=(i.user.screen_name+'\n')
+        mensaje_correo+=(i.text+'\n\n')
+    
+    print (mensaje_correo)
+    
+    mensaje_correo=mensaje_correo.encode('utf-8') 
+    
+    message = 'Subject: %s\n\n%s' % ('SUBJECT', mensaje_correo)
+    
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(correoSalida,password)
+    
+    
+    server.sendmail('Eduardo G', correoDestino, message)
+    server.quit()
+
+except tweepy.TweepError as e:
+    print (e)
+    print ('Error! Failed to get request token.')
+    
+    
+    
+    
