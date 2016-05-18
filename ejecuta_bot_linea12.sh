c3d7a5bdc21d33dd9cdf45c969d3cd2c05a9a962
Eduardo G - odraudek99 <odraudek99@gmail.com>
2016-05-17 22:21:10 -0500

    shell para ejecutar bots

---
+++ ejecuta_bot_linea12.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+cd /home/odraudek99/bots
+python2.7 bot_twitter_topics.py metro "linea12 OR linea dorada OR lineaDorada"
---
+++ ejecuta_bot_users.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+cd /home/odraudek99/bots
+echo jaja > log1.log
+python2.7  bot_twitter_users.py
